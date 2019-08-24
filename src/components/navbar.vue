494940e4bb43b440d651ae757634d968ccfc5a3f
Abeer Khalaf <abeerkhalaf96@gmail.com>
2019-08-24 16:35:03 -0400

    w

---
+++ src/components/navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav>
+       <v-toolbar flat app>
+          <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+<v-toolbar-title class="text-uppercase grey--text">
+    <span class="font-weight-light">China</span>
+    <span>Doto</span>
+</v-toolbar-title>
+           <v-spacer></v-spacer>
+           <v-btn flat color="grey lighten-4">
+               <span>Sign out</span>
+               <v-icon right>exit_to_app</v-icon>
+           </v-btn>
+       </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer"  absolute temporary class="primary">
+            <v-list-item  v-for="link in links" :key="link.text" router :to="link.route">
+               <v-list-tile-action>
+                    <v-icon class="white--text">{{link.icon}}</v-icon>
+                </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+                    </v-list-tile-content>
+            </v-list-item>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer:false,
+                links: [
+                    {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                    {icon: 'folder', text: 'My Projects', route: '/projects'},
+                    {icon: 'person', text: 'Team', route: '/team'},
+                ]
+            }
+        }
+
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  <h1>Homepage</h1>
+  <p class="red white--text">All hail mother china they will win TI9</p>
+  <p class="pink lighten-4 red--text text--darken-4">All hail mother china they will win TI9</p>
+  <h1 class="display-4">ALL HAIL THE TREES</h1>
+  <h4 class="display-1">-cblt</h4>
+  <p class="headline">FY GOD</p>
+  <p class="subheading font-weight-bold">FY GOD</p>
+  <p class="caption text-lowercase font-italic">Ni hao kailan</p>
+  <v-btn class="red white--text">Click Me</v-btn>
+  <v-btn  depressed color="pink">Click Me</v-btn>
+  <v-btn flat color="red">Click Me</v-btn>
+
+  <v-btn depressed class="red white--text">
+    <v-icon left>email</v-icon>
+    <span>give china your info</span>
+  </v-btn>
+
+  <v-btn depressed small class="red white--text">
+    <v-icon left small>email</v-icon>
+    <span>give china your info</span>
+  </v-btn>
+
+  <v-btn depressed large class="red white--text">
+    <span>give china your info</span>
+    <v-icon right large>email</v-icon>
+  </v-btn>
+
+  <v-btn fab depressed small dark color="purple">
+    <v-icon>favorite</v-icon>
+  </v-btn>
+
+  <v-btn class="hidden-md-and-up">click me</v-btn>
+  <v-btn class="hidden-sm-only">click me</v-btn>
+
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
